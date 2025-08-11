53171d7f03589a6c088435f3595a59a1a92a5327
Umakatakam12 <Ukatakam12@gmail.com>
2025-08-11 11:53:42 +0530

    Assignment 1

---
+++ src/Assignment/Assignment1_DataTypes.java
@@ -0,0 +1,10 @@
+package Assignment;
+
+public class Assignment1_DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/language/basics/DebugProgram.java
@@ -0,0 +1,16 @@
+package language.basics;
+
+public class DebugProgram {
+
+	public static void main(String[] args) {
+     int i=1;
+     System.out.println("step1: Program Starts");
+     System.out.println("step2: Preparing execution for next steps");
+     System.out.println("step3: Performing calculations");
+     int j = i+1;
+     System.out.println(i / j);
+     System.out.println("step5: Program execution completed");
+
+	}
+
+}
